ddba065f8c0caa8b206ee00775d046b690897c90
Zachary Huff <zach.huff.386@gmail.com>
2014-04-05 04:01:19 -0400

    Use cwd for data dirs

--- resources.go
+++ resources.go
@@ -5,8 +5,6 @@ import (
 	"log"
 	"os"
 	"path/filepath"
-
-	"bitbucket.org/kardianos/osext"
 )
 
 // exists returns whether the given file or directory exists or not
@@ -22,13 +20,8 @@ func exists(path string) bool {
 }
 
 func resourcePaths() (staticPath string, dataPath string) {
-	base, err := osext.ExecutableFolder()
-	if err != nil {
-		log.Fatal("Could not read base dir")
-	}
-
-	staticPath = filepath.Join(base, "static")
-	dataPath = filepath.Join(base, "data")
+	staticPath = "static"
+	dataPath = "data"
 	if exists(dataPath) && exists(staticPath) {
 		return
 	}
